7069e2676e62be524f4cfedb8d7f1494c2cc6699
Saad Arqam <saadarqam@Saads-MacBook-Pro.local>
2025-04-14 19:40:05 +0530

    changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({title,id,rightIcon,leftIcon,containerClass}) => {
+const Button = ({title,id,leftIcon,containerClass}) => {
   return (
       <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full 
         bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
